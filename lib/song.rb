89184905612f2e4cf235f9080aabf381f67f5b80
John Hayes <johnnyhayes7@gmail.com>
2019-02-04 16:59:21 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@genre_count = {}
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count 
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq 
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+     @@genres.each do |i|
+       if @@genre_count.key.include?(i) == false
+          @@genre_count = {@genre => 1}
+       else
+         @@genre_count[@genre] = @@genre_count.values += 1
+       end
+     end
+    
+  end
+  
+  
+end
